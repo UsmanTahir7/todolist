c6d389ae1a36abcc58aeadf2d92e2901db886615
Usman Tahir <65866570+UsmanTahir7@users.noreply.github.com>
2022-10-13 01:46:19 -0400

    completed 100%

--- app.js
+++ app.js
@@ -73,6 +73,11 @@ app.post("/", function(req, res) {
     res.redirect("/");
 });
 
-app.listen(3000, function (){
-    console.log("server started on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+ 
+app.listen(port, function() {
+  console.log("Server started succesfully");
+});   
